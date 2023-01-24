46d8c30410d2bf0a6d7c3b2313ad72b830324b9f
Lexy Andati <andatilexy@gmail.com>
2023-01-24 11:41:39 +0200

    Fix something

--- fits_related/simple-mask.py
+++ fits_related/simple-mask.py
@@ -1,12 +1,21 @@
 import argparse
+import os
 import numpy as np
+import astropy.units as u
+from itertools import zip_longest
 
 from astropy.io import fits
 from astropy.coordinates import SkyCoord, FK5
-import astropy.units as u
-
 from astropy.wcs import WCS
+from scipy.ndimage import rotate
+
 from regions import (Regions, CircleSkyRegion, RectangleSkyRegion)
+# CircleAnnulusSkyRegion, EllipseSkyRegion, EllipseAnnulusSkyRegion
+# LineSkyRegion, PolygonSkyRegion, PointSkyRegion, 
+# RectangleAnnulusSkyRegion, RectangleAnnulusSkyRegion
+
+
+import matplotlib.pyplot as plt
 
 from ipdb import set_trace
 # from scrappy
@@ -52,23 +61,42 @@ def world_to_pixel_coords(ra, dec, wcs_ref):
         wcs = get_wcs(wcs_ref)
     else:
         wcs = wcs_ref
-
     world_coord = FK5(ra=ra, dec=dec)
     skies = SkyCoord(world_coord)
     x, y = skies.to_pixel(wcs)
-    return int(x), int(y)
+    return int(np.round(x,0)), int(np.round(y,0))
 
 #######################################################
 
-def cumulate_regions(fname, data, reg, buffer):
+
+def merotate2(img, angle, pivot):
+    """
+    From:
+    https://stackoverflow.com/questions/25458442/rotate-a-2d-image-around-specified-origin-in-python
+    """
+    padx = [img.shape[1] - pivot[0], pivot[0]]
+    pady = [img.shape[0] - pivot[1], pivot[1]]
+    paded = np.pad(img, [pady, padx], 'constant')
+    rotated = rotate(paded, angle, reshape=False)
+    return rotated[pady[0]:-pady[1], padx[0]:-padx[1]]
+
+
+def cumulate_regions(fname, data, reg):
+    buffer = np.zeros(data.shape)
+
+    # todo: Add support for other types of regions
+    if not hasattr(reg, "center"):
+        print("This region has no center, skipping")
+        return buffer
+
     cx, cy = world_to_pixel_coords(reg.center.ra, reg.center.dec, wcs_ref=fname)
     x_npix, y_npix = data.shape
     w,h = None, None
-
+    
     if isinstance(reg, CircleSkyRegion):
         w, h = [int(reg.radius.value)]*2
     elif isinstance(reg, RectangleSkyRegion):
-        w, h = int(reg.width.value), int(reg.height.value)
+        w, h = int(reg.width.value)//2, int(reg.height.value)//2
     
     if w is not None:
         minx, maxx = cx-w, cx+w
@@ -76,12 +104,16 @@ def cumulate_regions(fname, data, reg, buffer):
 
         xs = np.ma.masked_outside(np.arange(minx, maxx), 0, x_npix).compressed()
         ys = np.ma.masked_outside(np.arange(miny, maxy), 0, y_npix).compressed()
-        mesh = tuple(np.meshgrid(xs, ys, sparse=True))
+
+        # notice how we pass y before x, returns x before y
+        # Leave it this way!!!!
+        mesh = tuple(np.meshgrid(ys, xs))
         buffer[mesh] = 1
+        
+        if hasattr(reg, "angle"):
+            pivot = cx, cy
+            buffer = merotate2(buffer, -reg.angle.value, pivot=pivot)        
     return buffer
-
-
-
 
 def make_mask(fname, outname, above=None, below=None, regname=None):
     """
@@ -115,46 +147,29 @@ def make_mask(fname, outname, above=None, below=None, regname=None):
 
     if regname is not None:
       
-        buff = np.zeros(data.shape)
+        finale = np.zeros(data.shape)
         regs = Regions.read(regname, format="ds9")
 
         for reg in regs:
-            buff = cumulate_regions(fname, data, reg, buff)
+            finale += cumulate_regions(fname, data, reg)
 
 
-
-        # x_npix, y_npix = data.shape
-        # w,h = None, None
-
-        # reg, = Regions.read(regname, format="ds9")
-        # cx, cy = world_to_pixel_coords(reg.center.ra, reg.center.dec, wcs_ref=fname)
-        # if isinstance(reg, RectangleSkyRegion):
-        #     w, h = [reg.radius.value]*2
-        # elif isinstance(reg, CircleSkyRegion):
-        #     w, h = reg.width.value, reg.height.value
-        
-        # if w is not None:
-        #     buff = np.zeros(x_npix, y_npix)
-        #     minx, maxx = cx-w, cx+w
-        #     miny, maxy = cy-h, cy+h
-
-        #     xs = np.ma.masked_outside(np.arange(minx, maxx), 0, x_npix).compressed()
-        #     ys = np.ma.masked_outside(np.arange(miny, maxy), 0, y_npix).compressed()
-        #     mesh = tuple(np.meshgrid(xs, ys, sparse=True))
-        #     buff[mesh] = 1
-
-        if buff.sum() == 0:
+        if finale.sum() == 0:
             print("Invalid region(s). We're ignoring this")
         else:
-            mask = buff * mask
-            
+            mask = finale * mask
+        plt.imshow(finale + mask, origin="lower")
+        ylim, xlim = np.where(mask+finale == 1)
+        plt.xlim(np.min(xlim), np.max(xlim))
+        plt.ylim(np.min(ylim), np.max(ylim))
+        plt.savefig(outname+"-overlay.png")
 
     outname += ".mask.fits" if ".fits" not in outname else ""
 
     print(f"Writing output mask into {outname}")
     fits.writeto(outname, mask, header=hdr, overwrite=True)
 
-    return mask
+    return
 
 
 def parser():
@@ -162,28 +177,37 @@ def parser():
     parse.add_argument("iname",
         help="Input image from which to generate mask")
     parse.add_argument("-o", "--outname", dest="oname", 
-        default="newmax.mask.fits", metavar="", type=str,
-        help="Where to dump the output")
+        default=[], metavar="", type=str, nargs="+",
+        help="Where to dump the output(s). We can iterate over multiple reg files. See '-rb' ")
     parse.add_argument("-above", "--above", dest="above", default=None,
         metavar="", type=float,
         help="Valuse above which to mask")
     parse.add_argument("-below", "--below", dest="below", default=None,
         metavar="", type=float,
         help="Values below which to mask")
-    parse.add_argument("-rb", "--region-bound", dest="regname",default=None,
-        metavar="", type=str,
-        help="DS9 region file within which to make our mask")
+    parse.add_argument("-rb", "--region-bound", dest="regname", default=[],
+        metavar="", type=str, nargs="+",
+        help="DS9 region file(s) within which to make our mask")
     return parse
 
 
 def main():
     opts = parser().parse_args()
-    mask = make_mask(opts.iname, opts.oname, above=opts.above, below=opts.below,
-        regname=opts.regname)
+
+    for i, oname, regname in enumerate(zip_longest(opts.oname, opts.regname)):
+        if oname is None:
+            oname = f"output-mask-{i}.fits"
+        make_mask(opts.iname, oname, above=opts.above, below=opts.below,
+            regname=regname)
     print('------- Done ------')
 
 if __name__ == "__main__":
     main()
     """
     python simple-mask.py ../../6-00-polarimetry/i-mfs.fits -o testing -above 4e-3 -rb pica_region-for-mask.reg
+
+    python simple-mask.py ../6-00-polarimetry/i-mfs.fits -o east-lobe.fits -above 4e-3 -rb important_regions/lobes/e-lobe.reg
+
+    python simple-mask.py ../6-00-polarimetry/i-mfs.fits -o west-lobe.fits -above 4e-3 -rb important_regions/lobes/w-lobe.reg
+
     """
